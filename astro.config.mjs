2714e4af354566231305e9713f300912c2f57c6d
Frank Showalter <842058+fshowalter@users.noreply.github.com>
2024-07-14 23:18:29 -0400

    wip

--- astro.config.mjs
+++ astro.config.mjs
@@ -20,7 +20,10 @@ export default defineConfig({
       alias: {
         // your aliases should mirror the ones defined in your tsconfig.json
         // i'll re-use the ones in the official docs:
-        "@": path.resolve(path.dirname(""), "./src"),
+        "@/components": path.resolve(path.dirname(""), "./src/components"),
+        "@/layouts": path.resolve(path.dirname(""), "./src/layouts"),
+        "@/utils": path.resolve(path.dirname(""), "./src/utils"),
+        "@/api": path.resolve(path.dirname(""), "./src/api"),
       },
     },
   },
