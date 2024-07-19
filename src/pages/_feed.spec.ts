00c94c71014445ccdfd939923edecb656741e696
Frank Showalter <842058+fshowalter@users.noreply.github.com>
2024-07-19 07:29:13 -0400

    wip

--- src/pages/_feed.spec.ts
+++ src/pages/_feed.spec.ts
@@ -4,7 +4,7 @@ import { describe, it } from "vitest";
 import * as FeedEndpoint from "./feed.xml.ts";
 
 describe("/feed.xml", () => {
-  it("matches snapshot", { timeout: 20000 }, async ({ expect }) => {
+  it("matches snapshot", { timeout: 40000 }, async ({ expect }) => {
     const container = await AstroContainer.create();
 
     // @ts-expect-error astro signature is wrong
